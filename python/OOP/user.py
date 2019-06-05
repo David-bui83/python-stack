d519373ced92e1a6c468db5ef6a502e37b99e745
Mona Lisa <david.t.bui83@gmail.com>
2019-06-04 20:38:16 -0700

    methods chaining

--- python/OOP/user.py
+++ python/OOP/user.py
@@ -5,6 +5,7 @@ class User:
 
   def make_deposit(self, d_amount):
     self.account += d_amount
+    return self
   
   def make_withdraw(self, w_amount):
     if w_amount < self.account:
@@ -12,40 +13,27 @@ class User:
       print(self.account)
     else: 
       print('Insufficient fund')
-
+    return self
   def display_user_balance(self):
     print(f"{self.name} balance is {self.account}")
+    return self
 
   def transfer(self, name, amount):
     if amount < self.account:
       self.account -= amount
       name.make_deposit(amount)
-
     else:
       print('Insufficient fund')
+    return self
 
 jack = User('Jack')
 jane = User('Jane')
 jim = User('Jim')
 
-jack.make_deposit(1000)
-jack.make_deposit(1000)
-jack.make_deposit(1000)
-jack.make_withdraw(500)
-jack.display_user_balance()
+jack.make_deposit(1000).make_deposit(1000).make_deposit(1000).make_withdraw(500).display_user_balance()
 
-jane.make_deposit(2000)
-jane.make_deposit(2000)
-jane.make_deposit(500)
-jane.make_deposit(500)
-jane.display_user_balance()
+jane.make_deposit(2000).make_deposit(2000).make_deposit(500).make_deposit(500).display_user_balance()
 
-jim.make_deposit(1000)
-jim.make_withdraw(500)
-jim.make_withdraw(200)
-jim.make_withdraw(400)
-jim.display_user_balance()
+jim.make_deposit(1000).make_withdraw(500).make_withdraw(200).make_withdraw(400).display_user_balance()
 
-jack.transfer(jane, 500)
-jack.display_user_balance()
-jane.display_user_balance()
+jack.transfer(jane, 500).display_user_balance().display_user_balance()
