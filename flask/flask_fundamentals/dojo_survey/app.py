4df588a52ddacb85402629f09e78df80aa3f1bd7
Mona Lisa <david.t.bui83@gmail.com>
2019-06-08 13:51:16 -0700

    dojo fruit store

--- flask/flask_fundamentals/dojo_survey/app.py
+++ flask/flask_fundamentals/dojo_survey/app.py
@@ -8,7 +8,8 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-  
+  users = []
+
   in_dic = {
     "first_name": '',
     "last_name": '',
@@ -20,73 +21,41 @@ def result():
     'online': '', 
     'comments': ''
   }
-  users = [in_dic]
-
+  
   f_name = request.form.get('first_name')
   in_dic['first_name'] = checkForEmpty(f_name)
 
   l_name = request.form.get('last_name')
   in_dic['last_name'] = checkForEmpty(l_name)
-  # if f_name:
-  #   in_dic['last_name'] = l_name
-  # else:
-  #   in_dic['last_name'] = False
 
   email = request.form.get('email')
   in_dic['email'] = checkForEmpty(email)
-  # if f_name:
-  #   in_dic['email'] = email
-  # else:
-  #   in_dic['email'] = False
 
   check1 = request.form.get('check1')
   in_dic['check1'] = checkForEmpty(check1)
-  # if check1:
-  #   in_dic['check1'] = check1
-  # else: 
-  #   in_dic['check1'] = False
 
   check2 = request.form.get('check2')
   in_dic['check2'] = checkForEmpty(check2)
-  # if check2:
-  #   in_dic['check2'] = check2
-  # else: 
-  #   in_dic['check2'] = False
 
   check3 = request.form.get('check3')
   in_dic['check3'] = checkForEmpty(check3)
-  # if check3:
-  #   in_dic['check3'] = check3
-  # else: 
-  #   in_dic['check3'] = False
 
   check4 = request.form.get('check4')
   in_dic['check4'] = checkForEmpty(check4)
-  # if check4:
-  #   in_dic['check4'] = check4
-  # else: 
-  #   in_dic['check4'] = False
 
   online = request.form.get('online')
   in_dic['online'] = checkForEmpty(online)
-  # if online:
-  #   in_dic['online'] = online
-  # else: 
-  #   in_dic['online'] = False
-
-  # if in_person:
-  #   in_dic['in_person'] = in_person
-  # else: 
-  #   in_dic['in_person'] = False
 
   comments = request.form.get('comments')
   in_dic['comments'] = checkForEmpty(comments)
-  # if comments:
-  #   in_dic['comments'] = comments
-  # else: 
-  #   in_dic['comments'] = False
+  
+  users.append(in_dic)
 
   return render_template('result.html', users = users)
+
+@app.route('/return', methods=['GET'])
+def backToIndex():
+  return render_template('index.html')
 
 @app.errorhandler(404)
 def something_is_worng(e):
--- flask/flask_fundamentals/dojo_survey/sub/checkinput.py
+++ flask/flask_fundamentals/dojo_survey/sub/checkinput.py
@@ -1,20 +1,8 @@
-# from flask import request
-# class CheckInput:
-#   def __init__(self):
-#     self.value = None
-  
-#   def checkForEmpty(self, val):
-#     if len(val) != 0:
-#       self.value  = val
-#       return self.value
-#     else:
-#       self.val = ''
-#       return self.value
 
 def checkForEmpty(val=''):
   if (val):
     value  = val
     return value
   else:
-    value = False
+    value = ''
     return value
