6b681b125e83f267d6e77f916024ebd7ee8b7be2
Mona Lisa <david.t.bui83@gmail.com>
2019-06-08 17:22:45 -0700

    counter

---
+++ flask/flask_fundamentals/counter/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'super'
+
+@app.route('/')
+def index():
+  
+  if not 'visited' in session:
+    session['visited'] = 0
+  else:
+    session['visited'] += 1
+  
+  return render_template('index.html')
+
+@app.route('/show')
+def show():
+  if 'visited' in session:
+    session['visited'] += 1
+  return redirect('/')
+
+@app.route('/reset')
+def delete():
+  session.clear()
+  return redirect('/')
+
+@app.route('/add_input', methods=['POST'])
+def addInput():
+ 
+  var = request.form.get('number')
+  if var:
+    session['visited'] += int(var) - 1
+    
+  return redirect('/')
+
+if __name__ == '__main__':
+  app.run(debug=True)
