191c66207ba441a72c083e3bca3003935ed6f746
Mona Lisa <david.t.bui83@gmail.com>
2019-06-17 19:04:30 -0700

    refactored ninja gold - django

--- django/django_intro/ninja_gold/apps/gold/urls.py
+++ django/django_intro/ninja_gold/apps/gold/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index),
-  url(r'^process_money$', views.process_money),
+  url(r'^process_money/(?P<name>\w+)$', views.process_money),
   url(r'^reset$', views.reset)
 ]
--- django/django_intro/ninja_gold/apps/gold/views.py
+++ django/django_intro/ninja_gold/apps/gold/views.py
@@ -6,13 +6,14 @@ from datetime import datetime
 def index(request):
   return render(request, 'index.html')
 
-def process_money(request):
+def process_money(request, name):
+  print(name)
   if not 'gold' in request.session:
     request.session['gold'] = 0 
   if not 'act_out' in request.session:
     request.session['act_out'] = []
 
-  print(request.POST['name'])
+  # print(request.POST['name'])
 
   building = {
     'farm': (10,20),
@@ -20,20 +21,21 @@ def process_money(request):
     'house': (2,5),
     'casino': (-50,50)
   }
+
   
-  gold = random.randint(building[request.POST['name']][0], building[request.POST['name']][1])
+  
+  gold = random.randint(building[name][0], building[name][1])
   now = datetime.time(datetime.now())
 
   if gold < 0:
-    str_out = f"<p class='red'>Entered a {request.POST['name']} and lost {gold}! ({now})</p>"
+    str_out = f"<p class='red'>Entered a {name} and lost {gold}! ({now})</p>"
     request.session['gold'] -= gold
   else:
-    str_out = f"<p class='green'>Earned {gold} golds from the {request.POST['name']}! ({now}) </p>"
+    str_out = f"<p class='green'>Earned {gold} golds from the {name}! ({now}) </p>"
     request.session['gold'] += gold  
   print(gold)
 
   request.session['act_out'].append(str_out)
-  print(request.session['act_out'])
   return redirect('/')
 
 def reset(request):
