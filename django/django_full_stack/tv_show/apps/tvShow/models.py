0147f835a16ee1aa472b05d183247e80180bdb44
Mona Lisa <david.t.bui83@gmail.com>
2019-06-20 13:17:07 -0700

    validation for tv shows

--- django/django_full_stack/tv_show/apps/tvShow/models.py
+++ django/django_full_stack/tv_show/apps/tvShow/models.py
@@ -1,4 +1,22 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import datetime
+
+class ShowManager(models.Manager):
+  def basic_validator(self, postData):
+    errors = {}
+    d = datetime.now()
+
+    if len(postData['title']) < 2:
+      errors['title'] = 'Title must be at least two characters'
+    if len(postData['network']) < 3:
+      errors['network'] = 'Network needs to be at least 3 characters'
+    if len(postData['desc']) > 1 and len(postData['desc']) < 10:
+      errors['desc'] = 'Description can be blank or must be at least 10 characters'
+    if datetime.strptime(postData['date'],'%Y-%m-%d') > d:
+      errors['date'] = 'Release date cannot be greater than current date'
+    
+    return errors
 
 # Create your models here.
 class Show(models.Model):
@@ -8,14 +26,24 @@ class Show(models.Model):
   desc = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  objects = ShowManager()
 
   def __repr__(self):
     return f"{self.title}"
 
-# class Movie(models.Model):
-#   title = models.CharField(max_length=45)
-#   description = models.TextField()
-#   release_date = models.DateTimeField()
-#   duration = models.IntegerField()
+# class BlogManager(models.Manager):
+#   def basic_validator(self, postData):
+#       errors = {}
+#       # add keys and values to errors dictionary for each invalid field
+#       if len(postData['name']) < 5:
+#           errors["name"] = "Blog name should be at least 5 characters"
+#       if len(postData['desc']) < 10:
+#           errors["desc"] = "Blog description should be at least 10 characters"copy
+#       return errors
+
+# class Blog(models.Model):
+#   name = models.CharField(max_length=255)
+#   desc = models.TextField()
 #   created_at = models.DateTimeField(auto_now_add=True)
 #   updated_at = models.DateTimeField(auto_now=True)
+#   objects = BlogManager()    # add this line!
--- django/django_full_stack/tv_show/apps/tvShow/views.py
+++ django/django_full_stack/tv_show/apps/tvShow/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from apps.tvShow.models import *
+from django.contrib import messages
 from datetime import datetime
 
 # Create your views here.
@@ -17,10 +18,24 @@ def show_new(request):
   return render(request, 'tvShow/show_new.html')
 
 def create_show(request):
-  
-  new_show = Show.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['date'],desc=request.POST['desc'])
 
-  return redirect('/shows/' + str(new_show.id))
+  errors = Show.objects.basic_validator(request.POST)
+  # print(request.POST['date'])
+  # print(datetime.strptime(request.POST['date'], '%Y-%m-%d'))
+  exist = Show.objects.filter(title=request.POST['title'])
+  if exist:
+    print('asdfasfasdf')
+    messages.error(request, 'Show already in database')
+    return redirect('/shows/new')
+  else:
+    if len(errors) > 0:
+      for key, value in errors.items():
+        messages.error(request, value)
+      return redirect('/shows/new')
+  
+    new_show = Show.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['date'],desc=request.POST['desc'])
+
+    return redirect('/shows/' + str(new_show.id))
 
 def display_show(request, id):
   context= {
@@ -36,13 +51,22 @@ def edit_show(request, id):
   return render(request, 'tvShow/edit.html', context)
 
 def make_edit(request, id):
-  edit_show = Show.objects.get(id=id)
 
-  edit_show.title = request.POST['title']
-  edit_show.network = request.POST['network']
-  edit_show.release_date = request.POST['date']
-  edit_show.desc = request.POST['desc']
-  edit_show.save()
+  errors = Show.objects.basic_validator(request.POST)
+  
+  if len(errors) > 0:
+    for key, value in errors.items():
+      messages.error(request, value)
+    return redirect('/shows/'+ str(id)+'/edit')
+  else:
+
+    edit_show = Show.objects.get(id=id)
+
+    edit_show.title = request.POST['title']
+    edit_show.network = request.POST['network']
+    edit_show.release_date = request.POST['date']
+    edit_show.desc = request.POST['desc']
+    edit_show.save()
 
   return redirect('/shows/'+ str(id))
 
